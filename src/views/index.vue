114f99fe34b79851c5d03a8f1584ff01592740d0
SunZhichao <2433469529@qq.com>
2019-10-15 08:10:10 +0800

    界面美化

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,14 +2,18 @@
     <div id="box">
         <div class="nav">
             <div class="img">
-                <img :src="imgsrc" alt="" v-on:click="img">
+                <img :src="imgsrc" alt="">
             </div>
             <div id="select">
                 <div id='selecttop'></div>
                 <div id="selectuser">
-                    <ul>
+                    <ul v-if="this.name!=undefined">
                         <li>更换头像</li>
-                        <li>退出登陆</li>
+                        <li  v-on:click="loginout">退出登陆</li>
+                    </ul>
+                    <ul v-else>
+                        <li><router-link to="/login" style="cursor:pointer;text-decoration: none;color: black;">登录</router-link></li>
+                        <li><router-link to="/register" style="cursor:pointer;text-decoration: none;color: black;">注册</router-link></li>
                     </ul>
                 </div>
             </div>
@@ -28,30 +32,44 @@
             <div class="list">
                 <div class="todolist">
                     <font>未执行</font>
-                    <div id="f">
-                        <ul>
-                            <li v-for="(item,index) in flaseitem" v-on:click="del" v-bind:ind="index" v-bind:bool=0>{{item}}</li>
-                        </ul>
+                    <div style="height: 49%;overflow: auto;overflow-x: hidden;">
+                            <ul>
+                                <li class='trueli' v-on:click="itemclick" v-for="(item,index) in flaseitem" v-bind:ind="index" v-bind:bool=0><div class='changeitem' v-on:click="changebool" v-bind:ind="index" v-bind:bool=0></div>{{item}} <div v-on:click="del" v-bind:ind="index" v-bind:bool=0 class="delitem">x</div></li>
+                            </ul>
                     </div>
-                    <hr>
                     <font>已执行</font>
-                  <div id="t">
-                      <ul>
-                          <li v-for="(item,index) in trueitem" v-on:click="del" v-bind:ind="index" v-bind:bool="1">{{item}}</li>
-                      </ul>
-                  </div>
+                    <div style="height: 43%;overflow: auto;overflow-x: hidden;">
 
+                            <ul>
+                                <li v-for="(item,index) in trueitem" v-bind:ind="index" v-bind:bool=1><div class='changeitem ' v-on:click="changebool" v-bind:ind="index" v-bind:bool=1>√</div><div class="truefont">{{item}}</div> <div v-on:click="del" v-bind:ind="index" v-bind:bool=1 class="delitem">x</div></li>
+                            </ul>
+                    </div>
                 </div>
             </div>
             <div class="item">
+                <div id="msgbox">
+                    <div class="msgs">
+                        <h1>0</h1>
+                        <font>预计时间(h)</font>
+                    </div>
+                    <div class="msgs">
+                        <h1>{{this.flaseitem.length}}</h1>
+                        <font>待完成任务</font>
+                    </div>
+                    <div class="msgs">
+                        <h1>0</h1>
+                        <font>已用时间</font>
+                    </div>
+                    <div class="msgs">
+                        <h1>{{this.trueitem.length}}</h1>
+                        <font>已完成任务</font>
+                    </div>
+                </div>
                 <div class="input">
-                    <input type="text" placeholder="请输入一个任务" v-model="item" v-on:keyup.13='creates'><button v-on:click="creates">提交</button>
+                    <input type="text" placeholder="请输入一个任务,按回车键或点击提交保存" v-model="item" v-on:keyup.13='creates'><button v-on:click="creates">提交</button>
                 </div>
                 <div class="todoitem">
-                    <ul>
-                        <li v-for="(item,index) in flaseitem"><input type="checkbox">{{item}} <button v-on:click="del" v-bind:ind="index" v-bind:bool=0>删除</button><button v-on:click="changebool" v-bind:ind="index" v-bind:bool=0>更改</button></li>
-                        <li v-for="(item,index) in trueitem"><input type="checkbox">{{item}} <button v-on:click="del" v-bind:ind="index" v-bind:bool=1>删除</button><button v-on:click="changebool" v-bind:ind="index" v-bind:bool=1>更改</button></li>
-                    </ul>
+
                 </div>
             </div>
         </div>
@@ -74,7 +92,7 @@
             }
         },
         methods:{
-            img:function(){
+            loginout:function(){
                 // this.imgsrc=require('../../image/头像/首页背景.jpg')
                 this.name=undefined;
                 this.trueitem='';
@@ -102,7 +120,7 @@
             creates:function(e){
                 if(this.name){
                     var self = this;
-                    this.flaseitem.push(this.item);
+                    this.flaseitem.unshift(this.item);
                     //发送item请求
                     var xhr = new XMLHttpRequest();
                     xhr.open('post','/api/register/item/');
@@ -168,14 +186,14 @@
                 if(e.target.getAttribute('bool')==0){
                     for(var i=0;i<this.flaseitem.length;i++){
                         if(e.target.getAttribute("ind") == i){
-                            this.trueitem.push(this.flaseitem[i])
+                            this.trueitem.unshift(this.flaseitem[i])
                             this.flaseitem.splice(i,1)
                         }
                     }
                 }else if(e.target.getAttribute('bool')==1){
                     for(var i=0;i<this.trueitem.length;i++) {
                         if(e.target.getAttribute("ind") == i){
-                            this.flaseitem.push(this.trueitem[i])
+                            this.flaseitem.unshift(this.trueitem[i])
                             this.trueitem.splice(i,1)
                         }
                     }
@@ -201,6 +219,15 @@
 
                 //发送结束
             },
+            itemclick:function(e){
+                for(var i=0;i<this.flaseitem.length;i++){
+                    if(e.target.getAttribute('ind')==i){
+                        document.getElementsByClassName('trueli')[i].style.backgroundColor='gray'
+                    }else{
+                        document.getElementsByClassName('trueli')[i].style.backgroundColor='rgb(243,244,246)'
+                    }
+                }
+            }
         },
         created() {
             if(this.name == undefined){
@@ -237,6 +264,21 @@
 </script>
 
 <style scoped>
+    .changeitem{
+        width: 25px;
+        height: 25px;
+        border: 1px solid black;
+        border-radius: 50%;
+        cursor:pointer;
+        text-align: center;
+        line-height: 25px;
+        font-size: 28px;
+        margin-right: 10px;
+    }
+    .truefont{
+        text-decoration: line-through;
+        color: gray;
+    }
     #selecttop{
         cursor:pointer;
         position: absolute;
@@ -244,14 +286,10 @@
         left: 35px;
         border-width: 10px;
         border-style: solid;
-        border-color:transparent transparent white transparent;
-        display:block;
-    }
-    #selectuser{
-        display:block;
+        border-color:transparent transparent darkgrey transparent;
     }
     #selectuser ul{
-        background-color:white;
+        background-color:darkgrey;
         list-style-type: none;
         position: absolute;
         top: 80px;
@@ -259,9 +297,15 @@
     }
     #selectuser ul li{
         padding: 5px 5px 10px 5px;
+        cursor: pointer;
+        text-align: center;
+        width: 80px;
+    }
+    #selectuser ul li:hover{
+        background: cornflowerblue;
     }
     #box{
-        background-image: url('../../image/首页背景.jpg');
+        background-color:white;
         position: fixed;
         width: 100%;
         height: 100%;
@@ -272,7 +316,6 @@
         z-index: 2;
     }
     .nav{
-        border: 1px solid gray;
         padding: 18px;
         display: flex;
         align-items: center;
@@ -287,6 +330,15 @@
         overflow: hidden;
         cursor:pointer;
     }
+    #select{
+        display: none;
+    }
+    .img:hover+#select{
+        display: block;
+    }
+    #select:hover{
+        display: block;
+    }
     .img img{
         width:100%;
         height:100%;
@@ -294,57 +346,73 @@
     .content{
         width: 100%;
         height: 87%;
-        margin-top: 90px;
         display: flex;
         justify-content: space-between;
-        border: 2px solid black;
+        background-color:rgb(243,244,246);
     }
     /*待办事项集合*/
 
-    .todolist{
-        width: 106%;
-        height: 103%;
-        overflow: auto;
-    }
+
     .list{
-        width: 35%;
+        width: 20%;
         height: 100%;
         overflow: hidden;
-        overflow-x: hidden;
-        border: 1px solid red;
+        /*overflow-x: hidden;*/
     }
+    .todolist{
+        width: 103%;
+        height: 100%;
+        background-color:white;
+          /*overflow: auto;*/
+      }
     .todolist ul li{
-        border: 1px solid black;
-        background-color:gray;
-        width: 100%;
+        background-color:rgb(245,245,245);
+        width: 90%;
+        margin: 5px auto;
+        border-radius: 10px;
         height: 30px;
         padding:10px 0;
         display: flex;
         align-items: center;
         list-style-type: none;
+        position: relative;
     }
-    #f{
-        width: 100%;
-        height: 50%;
-        overflow: auto;
-        overflow-x: hidden;
-    }
-    #t{
-        width: 100%;
-        height: 50%;  overflow: hidden;
-        overflow: auto;
-        overflow-x: hidden;
+    .todolist ul li .delitem{
+        font-size: 20px;
+        cursor: pointer;
+        position: absolute;
+        right: 8px;
     }
     .item{
-        width: 60%;
-        border: 1px solid green;
+        width: 78%;
     }
     .todoitem{
         width: 100%;
         height: 94%;
-        border: 1px solid red;
         overflow:auto;
         margin: 0 auto;
+    }
+    #msgbox{
+        width: 90%;
+        height: 70px;
+        margin: 20px auto;
+        background-color:white;
+        border-radius: 15px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+    }
+    #msgbox .msgs{
+        width: 10%;
+        height: 80%;
+    }
+    #msgbox .msgs font{
+        font-size: 12px;
+        color: gray;
+    }
+    #msgbox .msgs h1{
+        color: red;
     }
     .input{
         border: 1px solid gray;
@@ -361,7 +429,9 @@
         border: none;
         background: none;
     }
+
     .input button{
+        transition: all 1s;
         width: 20%;
         height: 100%;
         outline: none;
@@ -369,6 +439,9 @@
         background: none;
         cursor:pointer;
     }
+    .input button:hover{
+          background-color: orange;
+      }
     .todoitem ul li{
         display: flex;
         align-items: center;
